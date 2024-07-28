da7d061828fa83682fb7cb722f11ff9be2e47371
tonybastard <anthonydebenedet@gmail.com>
2024-07-27 23:53:28 -0400

    added js for webservers. need to develop logic for OS specific commands

--- frontend/script.js
+++ frontend/script.js
@@ -5,23 +5,30 @@ document.getElementById('generate').addEventListener('click', function() {
     const command = document.getElementById('command').value;
 
     let dockerfile = `FROM ${os}\n\n`;
-    dockerfile += `WORKDIR /app\n\n`;
-    dockerfile += `RUN apt-get update\n`;
-    dockerfile += `RUN useradd --create-home appuser\n\n`;
-    dockerfile += `USER appuser\n\n`;
+
     
     if (database !== 'none') {
         dockerfile += `# Database configuration\n`;
         dockerfile += `# Install ${database}\n\n`;
     }
 
+    dockerfile += `RUN apt-get update\n`;
+
+    if (webserver !== 'none') {
+        dockerfile += `RUN apt-get install -y ${webserver}`
+    }
+
     dockerfile += `EXPOSE 8000\n\n`;
     dockerfile += `CMD ${command}\n\n`;
-    dockerfile += `COPY app.py .\n\n`;
-    dockerfile += `ENV API_KEY=hi_mom\n\n`;
-    dockerfile += `LABEL version="1.0"\n\n`;
-    dockerfile += `HEALTHCHECK CMD curl --fail http://localhost:8000/ || exit 1\n\n`;
-    dockerfile += `VOLUME ["/data"]`;
+    
+// dockerfile += `WORKDIR /app\n\n`;
+// dockerfile += `RUN useradd --create-home appuser\n\n`;
+// dockerfile += `USER appuser\n\n`;
+ //   dockerfile += `COPY app.py .\n\n`;
+ //   dockerfile += `ENV API_KEY=hi_mom\n\n`;
+ //   dockerfile += `LABEL version="1.0"\n\n`;
+ //   dockerfile += `HEALTHCHECK CMD curl --fail http://localhost:8000/ || exit 1\n\n`;
+ //   dockerfile += `VOLUME ["/data"]`;
 
     document.getElementById('dockerfile-content').textContent = dockerfile;
 });
